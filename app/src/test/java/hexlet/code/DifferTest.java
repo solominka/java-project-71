fb3d2253a6f270eb04d74b9613756c804e8d8cf1
annkolesnikova <annkolesnikova@ozon.ru>
2024-11-06 16:07:00 +0300

    IT 9: add yaml

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -22,8 +22,19 @@ class DifferTest {
     void shouldGenerateDiffForComplicatedJsonFiles() throws Exception {
         ClassLoader classLoader = getClass().getClassLoader();
         var resp = Differ.generate(
-                Path.of(classLoader.getResource("input/object_values/file1.json").getPath()),
-                Path.of(classLoader.getResource("input/object_values/file2.json").getPath())
+                Path.of(classLoader.getResource("input/object_values_json/file1.json").getPath()),
+                Path.of(classLoader.getResource("input/object_values_json/file2.json").getPath())
+        );
+        var correct = Files.readString(Path.of(classLoader.getResource("output/complicated_correct.txt").getPath()));
+        Assertions.assertEquals(correct, resp);
+    }
+
+    @Test
+    void shouldGenerateDiffForComplicatedYamlFiles() throws Exception {
+        ClassLoader classLoader = getClass().getClassLoader();
+        var resp = Differ.generate(
+                Path.of(classLoader.getResource("input/object_values_yaml/file1.yml").getPath()),
+                Path.of(classLoader.getResource("input/object_values_yaml/file2.yml").getPath())
         );
         var correct = Files.readString(Path.of(classLoader.getResource("output/complicated_correct.txt").getPath()));
         Assertions.assertEquals(correct, resp);
